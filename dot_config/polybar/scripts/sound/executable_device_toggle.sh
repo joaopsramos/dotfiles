f77802e1a76ad0de4e4bf59bc776e04c02f8bf79
João Ramos <joaopsramos231@gmail.com>
2022-10-18 10:48:11 -0300

    checkpoint

---
+++ dot_config/polybar/scripts/sound/executable_device_toggle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ASTRO="alsa_output.usb-Astro_Gaming_Astro_MixAmp_Pro-00.analog-stereo"
+SPEAKERS="alsa_output.pci-0000_09_00.4.analog-stereo"
+
+CURRENT_SINK=$(pactl get-default-sink)
+
+if [[ $CURRENT_SINK == $SPEAKERS  ]]; then
+    SINK=$ASTRO
+    ACTION="#sound-output.hook.0"
+elif [[ $CURRENT_SINK == $ASTRO ]]; then
+    SINK=$SPEAKERS
+    ACTION="#sound-output.hook.1"
+fi
+
+pactl set-default-sink $SINK
+polybar-msg action $ACTION
