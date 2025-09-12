676240e3dbc9ee36b22f7051b305f0928ab4a08a
João Ramos <joaopsramos231@gmail.com>
2025-09-12 13:03:59 -0300

    add zed

---
+++ dot_config/zed/scripts/executable_open_alternate_file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file=$ZED_RELATIVE_FILE
+file_to_open=""
+
+if [[ "$file" == *.ex ]]; then
+    # Replace "lib/" with "test/"
+    alt_file="${file/lib\//test/}"
+    # Remove the file extension
+    alt_file="${alt_file%.*}"
+    # Append "_test.exs"
+    file_to_open="${alt_file}_test.exs"
+elif [[ "$file" == *_test.exs ]]; then
+    # Replace "test/" with "lib/"
+    alt_file="${file/test\//lib/}"
+    # Replace "_test.exs" with ".ex"
+    file_to_open="${alt_file%_test.exs}.ex"
+fi
+
+if [[ -n "$file_to_open" ]]; then
+    zeditor "$file_to_open"
+fi
